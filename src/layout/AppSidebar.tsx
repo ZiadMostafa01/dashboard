8e3a6423c3f4a643170315f4cf3a8d13d060c6ba
ZiadMostafa01 <ziadmostafa9866@gmail.com>
2026-03-03 20:09:38 +0200

    Update

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -283,7 +283,7 @@ const AppSidebar: React.FC = () => {
 
   return (
     <aside
-      className={`fixed top-0 left-0 z-50 flex flex-col h-screen bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 transition-all duration-300 ease-in-out border-r border-gray-200 
+      className={`fixed top-0 left-0 z-50 flex flex-col h-[100dvh] bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 transition-all duration-300 ease-in-out border-r border-gray-200 
         ${
           isExpanded || isMobileOpen
             ? "w-[290px]"
